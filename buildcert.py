f48a93f8eed38d1b14456a016194265665a35f7b
Sven Roederer <devel-sven@geroedel.de>
2016-01-14 12:39:30 +0100

    [buildcert] sign by correct issuer, add the same cert extensions - Basic Constraints - Netscape Comment - Subject Key Identifier - Authority Key Identifier - (Extended) Key Usage

--- buildcert.py
+++ buildcert.py
@@ -17,6 +17,12 @@ def create_cert(cert_name, cert_email):
     self-signed cert and keypair and write them into that directory.
     """
 
+    # get required CA-data
+    ca_cert_file = open('/tmp/ffca.crt', 'r')
+    ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, ca_cert_file.read())
+    ca_key_file = open('/tmp/ffca.key', 'r')
+    ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, ca_key_file.read())
+
     if True:
 
         # create a key pair
@@ -25,6 +31,7 @@ def create_cert(cert_name, cert_email):
 
         # create a self-signed cert
         cert = crypto.X509()
+        cert.set_version = 3
         cert.get_subject().C = "DE"
         cert.get_subject().ST = "Eastern Germany"
         cert.get_subject().L = "Berlin"
@@ -34,9 +41,21 @@ def create_cert(cert_name, cert_email):
         cert.set_serial_number(1000)
         cert.gmtime_adj_notBefore(0)
         cert.gmtime_adj_notAfter(10*365*24*60*60)
-        cert.set_issuer(cert.get_subject())
+        cert.set_issuer(ca_cert.get_subject())
         cert.set_pubkey(k)
-        cert.sign(k, 'sha1')
+
+        # create cert extensions
+        cert_ext = [
+            crypto.X509Extension('basicConstraints', False, 'CA:FALSE'),
+            crypto.X509Extension('nsComment', False, 'made for you with PyOpenSSL'),
+            crypto.X509Extension('subjectKeyIdentifier', False, 'hash', subject=cert),
+            crypto.X509Extension('authorityKeyIdentifier', False, 'keyid:always,issuer:always', issuer=ca_cert),
+            crypto.X509Extension('extendedKeyUsage', False, 'TLS Web Client Authentication'),
+            crypto.X509Extension('keyUsage', False, 'Digital Signature'),
+        ]
+        cert.add_extensions(cert_ext)
+
+        cert.sign(ca_key, 'sha1')
 
 	print "Certificate generated"
 	print crypto.dump_certificate(crypto.FILETYPE_TEXT, cert)
