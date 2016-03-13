35050974ba70d1dff2ef1992d23311db42035893
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-03-13 01:57:36 +0100

    config, buildcert: implement configurable parameters
    just move all relevant paramters into config.py to have them not hardcoded

--- config.py
+++ config.py
@@ -3,5 +3,17 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 SECRET_KEY = 'foobar'
 DIRECTORY = "tests/openvpn/easy-rsa/keys/"
-COMMAND_BUILD = "tests/openvpn/clients/openvpn-build-key"
 COMMAND_MAIL = "tests/openvpn/clients/openvpn-mail-key"
+
+CACERT_FILE = '/tmp/ffca.crt'
+CAKEY_FILE = '/tmp/ffca.key'
+NEWKEY_ALG = 'rsa'
+NEWKEY_SIZE = 1024
+NEWCERT_COUNTRY = "DE"
+NEWCERT_STATE = "Eastern Germany"
+NEWCERT_LOCATION = "Berlin"
+NEWCERT_ORGANIZATION = "Foerderverein Freie Netzwerke e.V."
+NEWCERT_DURATION = 10*365*24*60*60 # 10 years
+NEWCERT_COMMENT = b'made for you with PyOpenSSL'
+NEWCERT_SIGNDIGEST = "sha1"
+
